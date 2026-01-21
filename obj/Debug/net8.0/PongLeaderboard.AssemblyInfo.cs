818b0ca5b7ad342b891edf373d075b6462a9c0fd
Sisoni <cosplayers.help@gmail.com>
2026-01-21 12:44:35 +0200

    finish

--- obj/Debug/net8.0/PongLeaderboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/PongLeaderboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PongLeaderboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782bdeb3a8f9e028b2c17925a257dd5a094614f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5765c50399bb8191d85ac789772b772129801f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("PongLeaderboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PongLeaderboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
